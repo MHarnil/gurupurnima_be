b99b464f93736f6cfd6c38e4abe49e9f8c9d26de
harnil <mharnil@gmail.com>
2025-06-03 10:18:52 +0530

    model change

--- models/student.js
+++ models/student.js
@@ -1,17 +1,7 @@
 const mongoose = require('mongoose');
 
-// Define Counter schema for auto-increment
-const counterSchema = new mongoose.Schema({
-    id: { type: String, required: true, unique: true },
-    seq: { type: Number, default: 0 }
-});
-
-// Counter model
-const Counter = mongoose.model('Counter', counterSchema);
-
-// Student schema
 const studentSchema = new mongoose.Schema({
-    registerNumber: { type: String, unique: true },
+    registerNumber: { type: String, required: true, unique: true },
     firstName: { type: String, required: true },
     middleName: { type: String },
     lastName: { type: String, required: true },
@@ -20,27 +10,6 @@ const studentSchema = new mongoose.Schema({
     age: { type: Number, required: true },
     teachersFeeTaken: { type: String, enum: ['Yes', 'No'], required: true },
     willTeachersFeeBeTaken: { type: String, enum: ['Yes', 'No'], required: true }
-}, {
-    timestamps: true
-});
-
-// Auto-increment registerNumber before saving
-studentSchema.pre('save', async function (next) {
-    if (this.isNew) {
-        try {
-            const counter = await Counter.findOneAndUpdate(
-                { id: 'student_register' },
-                { $inc: { seq: 1 } },
-                { new: true, upsert: true }
-            );
-            this.registerNumber = counter.seq.toString(); // Example: '1', '2', '3', etc.
-            next();
-        } catch (err) {
-            next(err);
-        }
-    } else {
-        next();
-    }
 });
 
 module.exports = mongoose.model('Student', studentSchema);
