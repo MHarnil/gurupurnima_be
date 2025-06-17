098a526d877f2a5575c3db209e04e6975c31fe9b
harnil <mharnil@gmail.com>
2025-06-17 10:05:12 +0530

    color add

--- models/Adhyaksh.js
+++ models/Adhyaksh.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const adhyakshSchema = new mongoose.Schema({
     name: { type: String, required: true },
     number: { type: String, required: true },
-    gender: { type: String, enum: ['Male', 'Female', 'Other'], required: true }
+    gender: { type: String, enum: ['Male', 'Female', 'Other'], required: true },
+    color: { type: String, required: true }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Adhyaksh', adhyakshSchema);
