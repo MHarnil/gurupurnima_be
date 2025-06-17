687488c0aa5c2b38ab9435b69e4d6aa045d303cc
harnil <mharnil@gmail.com>
2025-06-17 15:16:25 +0530

    color remove in adhyaksh

--- models/Adhyaksh.js
+++ models/Adhyaksh.js
@@ -4,7 +4,6 @@ const adhyakshSchema = new mongoose.Schema({
     name: { type: String, required: true },
     number: { type: String, required: true },
     gender: { type: String, enum: ['Male', 'Female', 'Other'], required: true },
-    color: { type: String, required: true }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Adhyaksh', adhyakshSchema);
